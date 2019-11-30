87f78da1dbd45a9302dc9577160c3982e85dffec
Israr <https://Israr_Shahid@bitbucket.org>
2019-11-30 17:32:38 +0500

    rum x hub

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-'use strict';
+"use strict";
 
 // Imports dependencies and set up http server
-const
-  express = require('express'),
-  bodyParser = require('body-parser'),
+const express = require("express"),
+  bodyParser = require("body-parser"),
   app = express().use(bodyParser.json()); // creates express http server
-  var xhub = require('express-x-hub');
-
+var xhub = require("express-x-hub");
 
 // app.set('port', (process.env.PORT || 5000));
 // app.listen(app.get('port'));
 
-app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
+app.use(xhub({ algorithm: "sha1", secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
-var token = process.env.TOKEN || 'kinectro_webhook_token';
+// var token = process.env.TOKEN || 'kinectro_webhook_token';
 var received_updates = [];
 
-app.get('/', function(req, res) {
-  console.log("hit index url",req.body);
-  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+app.get("/", function(req, res) {
+  console.log("hit index url", req.body);
+  res.send("<pre>" + JSON.stringify(received_updates, null, 2) + "</pre>");
 });
 
-app.get(['/facebook', '/instagram'], function(req, res) {
-  console.log("/facebook,/instagram")
+app.get(["/facebook", "/instagram"], function(req, res) {
+  console.log("/facebook,/instagram");
   if (
-    req.query['hub.mode'] == 'subscribe' &&
-    req.query['hub.verify_token'] == token
+    req.query["hub.mode"] == "subscribe" &&
+    req.query["hub.verify_token"] == process.env.TOKEN
   ) {
-    res.send(req.query['hub.challenge']);
+    res.send(req.query["hub.challenge"]);
   } else {
     res.sendStatus(400);
   }
 });
 
-app.post('/facebook', function(req, res) {
-  console.log('Facebook request body:', req.body);
+app.post("/facebook", function(req, res) {
+  console.log("Facebook request body:", req.body);
 
   if (!req.isXHubValid()) {
-    console.log('Warning - request header X-Hub-Signature not present or invalid');
+    console.log(
+      "Warning - request header X-Hub-Signature not present or invalid"
+    );
     res.sendStatus(401);
     return;
   }
 
-  console.log('request header X-Hub-Signature validated');
+  console.log("request header X-Hub-Signature validated");
   // Process the Facebook updates here
   received_updates.unshift(req.body);
   res.sendStatus(200);
 });
 
-app.post('/instagram', function(req, res) {
-  console.log('instagram request body',req.body);
+app.post("/instagram", function(req, res) {
+  console.log("instagram request body", req.body);
   // Process the Instagram updates here
   received_updates.unshift(req.body);
   res.sendStatus(200);
@@ -58,63 +58,55 @@ app.post('/instagram', function(req, res) {
 
 // ------------------
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
 // app.set('port', (process.env.PORT || 5000), () => console.log('webhook is listening'));
 // app.listen(app.get('port'));
 
-// Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {  
- 
+// Creates the endpoint for our webhook
+app.post("/webhook", (req, res) => {
   let body = req.body;
 
   // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
+  if (body.object === "page") {
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
-
-      // Gets the message. entry.messaging is an array, but 
+      // Gets the message. entry.messaging is an array, but
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
     });
 
     // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
+    res.status(200).send("EVENT_RECEIVED");
   } else {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
-
 });
 
 // Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
-
+app.get("/webhook", (req, res) => {
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "kinectro_webhook_token"
-    
+  let VERIFY_TOKEN = "kinectro_webhook_token";
+
   // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
   console.log(mode);
   console.log(token);
   console.log(challenge);
-    
+
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
-  
     // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
       // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
+      console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
-    
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
+      res.sendStatus(403);
     }
   }
 });
