7ea3431f490a12b1cb83f3cf27019dbe5b814578
Israr <https://Israr_Shahid@bitbucket.org>
2019-12-01 15:46:40 +0500

    removed [0]

--- index.js
+++ index.js
@@ -68,9 +68,14 @@ app.post("/webhook", (req, res) => {
   if (body.object === "page") {
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
+      console.log(entry)
       // Gets the message. entry.messaging is an array, but
       // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
+      console.log("************")
+      console.log("************")
+      console.log("************")
+      console.log(entry.messaging)
+      let webhook_event = entry.messaging;
       console.log(webhook_event);
     });
 
