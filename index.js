4aafff51670b285158a5974fef792ec1d620d208
Israr <https://Israr_Shahid@bitbucket.org>
2019-12-01 18:07:21 +0500

    add more fields

--- index.js
+++ index.js
@@ -75,6 +75,11 @@ app.post("/webhook", (req, res) => {
       console.log("************");
       console.log("************");
       console.log(entry.changes[0].value.message);
+      console.log(entry.changes[0].value.post_id);
+      console.log(entry.changes[0].value.comment_id);
+      console.log(entry.changes[0].value.from.name);
+      console.log(entry.changes[0].value.post.permalink_url);
+      console.log(entry.changes[0].value.message);
       let webhook_event = entry.changes[0].value.message;
       console.log(webhook_event);
     });
